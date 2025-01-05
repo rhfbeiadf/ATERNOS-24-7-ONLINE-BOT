ffb9859c7ed0df95fa15d8834835d863046a4501
rhfbeiadf <priyanshuranjan282010@gmail.com>
2025-01-05 14:48:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host: 'C10C10Nbexx.aternos.me', 
+  host: 'hingland-LXup.aternos.me', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'NbexxAfkBot', 
-  port: 12344, 
+  username: 'server bot', 
+  port: 17267, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
